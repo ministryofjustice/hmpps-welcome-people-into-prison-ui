324e8be6580301ccd4a7dcf8dd0d98abf6e0e93b
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2021-08-31 09:35:26 +0100

    DCS-1100 Google Tag Manager (#7)
    * DCS-1100 Google Tag Manager
    GTM added together with CSP nonce.
    Created Dev and Preprod environments via GTM ui.
    Kubernetes secrets updated.
    
    * DCS-1100: changes following PR comments
    
    * DCS-1100 moving nonce creation
    
    Co-authored-by: andrewrlee <1517745+andrewrlee@users.noreply.github.com>

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express'
 import helmet from 'helmet'
+import crypto from 'crypto'
 
 export default function setUpWebSecurity(): Router {
   const router = express.Router()
@@ -7,18 +8,32 @@ export default function setUpWebSecurity(): Router {
   // Secure code best practice - see:
   // 1. https://expressjs.com/en/advanced/best-practice-security.html,
   // 2. https://www.npmjs.com/package/helmet
+
+  router.use((req, res, next) => {
+    res.locals.cspNonce = crypto.randomBytes(16).toString('base64')
+    next()
+  })
+
   router.use(
     helmet({
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
           // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
-          scriptSrc: ["'self'", 'code.jquery.com', "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='"],
+          scriptSrc: [
+            "'self'",
+            'www.googletagmanager.com',
+            'www.google-analytics.com',
+            'code.jquery.com',
+            "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+          ],
+          connectSrc: ["'self'", 'www.googletagmanager.com', 'www.google-analytics.com'],
           styleSrc: ["'self'", 'code.jquery.com'],
           fontSrc: ["'self'"],
         },
       },
     })
   )
+
   return router
 }
