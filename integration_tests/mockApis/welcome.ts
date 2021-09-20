7095d3ff004f47f4420c2e56b01788090bda395d
Louise Norris <louise.norris@digital.justice.gov.uk>
2021-09-20 17:10:58 +0100

    DCS-1198 added welcome api health check

--- integration_tests/mockApis/welcome.ts
+++ integration_tests/mockApis/welcome.ts
@@ -3,6 +3,19 @@ import incomingMovements from './responses/incomingMovemnts'
 import { stubFor } from './wiremock'
 
 export default {
+  stubPing: (): SuperAgentRequest => {
+    return stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/welcome/health/ping',
+      },
+      response: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        jsonBody: { status: 'UP' },
+      },
+    })
+  },
   stubIncomingMovements: (activeCaseLoadId: string): SuperAgentRequest => {
     return stubFor({
       request: {
