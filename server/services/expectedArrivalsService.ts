835507ffc1a21d41780ed2468f54c515a3bb9da9
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2021-09-30 14:46:05 +0100

    DCS-1200 location types renamed. fromLocationType added to Movement type.

--- server/services/expectedArrivalsService.ts
+++ server/services/expectedArrivalsService.ts
@@ -5,9 +5,9 @@ import { groupBy, compareByFullName } from '../utils/utils'
 import type { RestClientBuilder, WelcomeClient, HmppsAuthClient } from '../data'
 
 export enum LocationType {
-  COURT = 'FROM_COURT',
-  CUSTODY_SUITE = 'FROM_CUSTODY_SUITE',
-  ANOTHER_ESTABLISHMENT = 'FROM_ANOTHER_ESTABLISHMENT',
+  COURT = 'COURT',
+  CUSTODY_SUITE = 'CUSTODY_SUITE',
+  PRISON = 'PRISON',
   OTHER = 'OTHER',
 }
 
@@ -17,12 +17,12 @@ export default class ExpectedArrivalsService {
     private readonly welcomeClientFactory: RestClientBuilder<WelcomeClient>
   ) {}
 
-  private getFromLocationType(item: Movement): LocationType {
-    if (!item.moveType) return item.fromLocationType
+  private static getFromLocationType(item: Movement): LocationType {
+    if (!item.moveType && item.fromLocationType) return item.fromLocationType
     if (item.moveType === 'PRISON_REMAND') return LocationType.COURT
     if (item.moveType === 'PRISON_RECALL') return LocationType.CUSTODY_SUITE
     if (item.moveType === 'VIDEO_REMAND') return LocationType.CUSTODY_SUITE
-    if (item.moveType === 'PRISON_TRANSFER') return LocationType.ANOTHER_ESTABLISHMENT
+    if (item.moveType === 'PRISON_TRANSFER') return LocationType.PRISON
     return LocationType.OTHER
   }
 
@@ -32,9 +32,9 @@ export default class ExpectedArrivalsService {
     return expectedArrivals.sort(compareByFullName)
   }
 
-  public async getArrivalsForToday(agencyId: string, now = () => moment()): Promise<Map<string, Movement[]>> {
+  public async getArrivalsForToday(agencyId: string, now = () => moment()): Promise<Map<LocationType, Movement[]>> {
     const expectedArrivals = await this.getExpectedArrivals(agencyId, now())
-    return groupBy(expectedArrivals, (arrival: Movement) => this.getFromLocationType(arrival))
+    return groupBy(expectedArrivals, (arrival: Movement) => ExpectedArrivalsService.getFromLocationType(arrival))
   }
 
   public async getImage(prisonNumber: string): Promise<Readable> {
