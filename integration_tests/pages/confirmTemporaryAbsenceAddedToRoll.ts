d34d8e5e9f5ad3d4998595846a65690631450258
Louise Norris <louise.norris@digital.justice.gov.uk>
2022-01-14 12:42:02 +0000

    DCS-1286 updated integration tests for temporary absences

--- integration_tests/pages/confirmTemporaryAbsenceAddedToRoll.ts
+++ integration_tests/pages/confirmTemporaryAbsenceAddedToRoll.ts
@@ -1,12 +1,16 @@
-import Page from './page'
+import Page, { PageElement } from './page'
 
 export default class ConfirmTemporaryAbsenceAddedToRollPage extends Page {
   constructor() {
-    super('is on the establishment roll and is located in reception')
+    super('has returned to Moorland (HMP & YOI)')
   }
 
   static goTo(id: string): ConfirmTemporaryAbsenceAddedToRollPage {
     cy.visit(`/prisoners/${id}/prisoner-returned`)
     return Page.verifyOnPage(ConfirmTemporaryAbsenceAddedToRollPage)
   }
+
+  viewEstablishmentRoll = (): PageElement => cy.get(`[data-qa=view-establishment-roll]`)
+
+  backToDigitalPrisonServices = (): PageElement => cy.get('[data-qa=back-to-dps]')
 }
