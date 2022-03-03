6064d4bca0fd9e9007ac5b4d1efe5814be1cd607
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2022-03-03 09:18:44 +0000

    DCS-1477 Handling rejected promises (#155)
    Not wrapping this in async handlers causes the process to terminate
    
    Co-authored-by: Louise N <louise.norris@digital.justice.gov.uk>

--- server/routes/bookedtoday/index.ts
+++ server/routes/bookedtoday/index.ts
@@ -5,13 +5,14 @@ import type { Services } from '../../services'
 
 import transferRoutes from './transfers'
 import arrivalRoutes from './arrivals'
+import asyncMiddleware from '../../middleware/asyncMiddleware'
 
 export default function routes(services: Services): Router {
   const router = express.Router()
 
   const choosePrisonerController = new ChoosePrisonerController(services.expectedArrivalsService)
-  router.get('/confirm-arrival/choose-prisoner', [choosePrisonerController.view()])
-  router.get('/confirm-arrival/choose-prisoner/:id', [choosePrisonerController.redirectToConfirm()])
+  router.get('/confirm-arrival/choose-prisoner', [asyncMiddleware(choosePrisonerController.view())])
+  router.get('/confirm-arrival/choose-prisoner/:id', [asyncMiddleware(choosePrisonerController.redirectToConfirm())])
 
   router.use(transferRoutes(services))
   router.use(arrivalRoutes(services))
