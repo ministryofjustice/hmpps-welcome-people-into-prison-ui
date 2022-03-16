3bc51166a76093f3a24120c4a5d63816f0b2f21a
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2022-03-16 07:20:06 +0000

    DCS-1462 display different content on /check-answers if new record to… (#172)
    * DCS-1462 display different content on /check-answers if new record to be created
    
    * DCS-1462 get prisonNumber from  data rather than moveData object
    
    * DCS-1462 change class name
    
    * DCS-1462 govukSummaryList nunjucks component in checkAnswers pages

--- integration_tests/pages/bookedtoday/arrivals/confirmArrival/checkAnswersForCreateNewRecord.ts
+++ integration_tests/pages/bookedtoday/arrivals/confirmArrival/checkAnswersForCreateNewRecord.ts
@@ -1,21 +1,21 @@
 import Page, { PageElement } from '../../../page'
 
-export default class CheckAnswersPage extends Page {
+export default class CheckAnswersForCreateNewRecordPage extends Page {
   constructor() {
-    super('Check your answers before adding')
+    super("You're about to add this person to the establishment roll")
   }
 
   name = (): PageElement => cy.get('.data-qa-prisoner-name')
 
   dob = (): PageElement => cy.get('.data-qa-dob')
 
-  prisonNumber = (): PageElement => cy.get('.data-qa-prison-number')
-
   pncNumber = (): PageElement => cy.get('.data-qa-pnc-number')
 
   sex = (): PageElement => cy.get('.data-qa-sex')
 
   reason = (): PageElement => cy.get('.data-qa-reason')
+
+  submissionParagraphTitle = () => cy.get('h2')
 
   addToRoll = (): PageElement => cy.get('[data-qa=add-to-roll]')
 }
