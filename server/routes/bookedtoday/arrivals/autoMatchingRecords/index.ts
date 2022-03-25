0947d0da6b4e6ccbbd6ef775eb965091c36c7069
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2022-03-25 08:57:16 +0000

    Update index.ts

--- server/routes/bookedtoday/arrivals/autoMatchingRecords/index.ts
+++ server/routes/bookedtoday/arrivals/autoMatchingRecords/index.ts
@@ -3,7 +3,6 @@ import SingleMatchingRecordFoundController from './singleMatchingRecordFoundCont
 import NoMatchingRecordsFoundController from './noMatchingRecordsFoundController'
 import MultipleExistingsRecordsFoundController from './multipleExistingRecordsFoundController'
 
-
 import authorisationForUrlMiddleware from '../../../../middleware/authorisationForUrlMiddleware'
 import asyncMiddleware from '../../../../middleware/asyncMiddleware'
 import type { Services } from '../../../../services'
