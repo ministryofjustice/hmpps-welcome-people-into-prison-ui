7b9bf74b44cd4dbf2a6023da13c43626df9ca603
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2022-03-25 10:08:28 +0000

    DCS-1498 Implementing multiple matches (#190)
    Also fixing html on multiple search records

--- server/routes/bookedtoday/arrivals/autoMatchingRecords/multipleExistingRecordsFoundController.ts
+++
@@ -1,9 +0,0 @@
-import type { RequestHandler } from 'express'
-
-export default class MultipleExistingRecordsFoundController {
-  public view(): RequestHandler {
-    return async (req, res) => {
-      return res.render('pages/bookedtoday/arrivals/autoMatchingRecords/multipleExistingRecordsFound.njk')
-    }
-  }
-}
---
+++ server/routes/bookedtoday/arrivals/autoMatchingRecords/multipleMatchingRecordsFoundController.ts
@@ -0,0 +1,45 @@
+import type { Request, RequestHandler, Response } from 'express'
+import { ExpectedArrivalsService } from '../../../../services'
+import { State } from '../state'
+
+export default class MultipleExistingRecordsFoundController {
+  constructor(private readonly expectedArrivalsService: ExpectedArrivalsService) {}
+
+  public view(): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { id } = req.params
+
+      const arrival = await this.expectedArrivalsService.getArrival(id)
+
+      return res.render('pages/bookedtoday/arrivals/autoMatchingRecords/multipleMatchingRecordsFound.njk', {
+        arrival: {
+          firstName: arrival.firstName,
+          lastName: arrival.lastName,
+          dateOfBirth: arrival.dateOfBirth,
+          prisonNumber: arrival.prisonNumber,
+          pncNumber: arrival.pncNumber,
+        },
+        data: { potentialMatches: arrival.potentialMatches, id },
+        errors: req.flash('errors'),
+      })
+    }
+  }
+
+  public submit(): RequestHandler {
+    return async (req: Request, res: Response) => {
+      const { id } = req.params
+
+      if (req.errors) {
+        req.flash('errors', req.body)
+        return res.redirect(`/prisoners/${id}/possible-matches-found`)
+      }
+
+      const { prisonNumber } = req.body
+      const selectedRecord = await this.expectedArrivalsService.getPrisonerDetails(prisonNumber)
+
+      State.newArrival.set(res, selectedRecord)
+
+      return res.redirect(`/prisoners/${id}/sex`)
+    }
+  }
+}
