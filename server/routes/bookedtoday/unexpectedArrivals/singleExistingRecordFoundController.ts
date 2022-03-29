d85124f8ad8dda8ed2655a7b51979ceca8499356
Louise Norris <louise.norris@digital.justice.gov.uk>
2022-03-29 15:47:25 +0100

    DCS-1525 stop cro number from displaying on single existing record found page

--- server/routes/bookedtoday/unexpectedArrivals/singleExistingRecordFoundController.ts
+++ server/routes/bookedtoday/unexpectedArrivals/singleExistingRecordFoundController.ts
@@ -4,11 +4,18 @@ import { State } from '../arrivals/state'
 export default class SingleMatchingRecordFoundController {
   public view(): RequestHandler {
     return async (req, res) => {
-      const data = State.searchDetails.get(req)
+      const potentialMatch = State.newArrival.get(req)
+      const searchData = State.searchDetails.get(req)
 
       return res.render('pages/unexpectedArrivals/singleExistingRecordFound.njk', {
-        data,
-        potentialMatch: State.newArrival.get(req),
+        data: {
+          firstName: searchData.firstName,
+          lastName: searchData.lastName,
+          dateOfBirth: searchData.dateOfBirth,
+          prisonNumber: searchData.prisonNumber,
+          pncNumber: searchData.pncNumber,
+        },
+        potentialMatch,
       })
     }
   }
