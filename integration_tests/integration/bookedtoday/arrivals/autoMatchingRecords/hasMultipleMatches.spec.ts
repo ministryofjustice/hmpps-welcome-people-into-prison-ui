2601ae8146c3a31046aba7d76a55fa768cf65d77
Louise Norris <louise.norris@digital.justice.gov.uk>
2022-04-13 16:19:58 +0100

    DCS-1491 updated validation message on imprisonment status page

--- integration_tests/integration/bookedtoday/arrivals/autoMatchingRecords/hasMultipleMatches.spec.ts
+++ integration_tests/integration/bookedtoday/arrivals/autoMatchingRecords/hasMultipleMatches.spec.ts
@@ -92,7 +92,7 @@ context('Arrival matches multiple records', () => {
 
     const imprisonmentStatusPage = Page.verifyOnPage(ImprisonmentStatusPage)
     imprisonmentStatusPage.continue().click()
-    imprisonmentStatusPage.hasError('Select a reason for imprisonment')
+    imprisonmentStatusPage.hasError('Select why this person is in prison')
     imprisonmentStatusPage.imprisonmentStatusRadioButton('determinate-sentence').click()
     imprisonmentStatusPage.continue().click()
 
--- server/middleware/validation/imprisonmentStatusesValidation.test.ts
+++ server/middleware/validation/imprisonmentStatusesValidation.test.ts
@@ -3,7 +3,7 @@ import validation from './imprisonmentStatusesValidation'
 describe('Imprisonment status validation middleware', () => {
   it('should return an error when a imprisonmentStatus is not selected', () => {
     const result = validation({ imprisonmentStatus: undefined })
-    expect(result).toEqual([{ text: 'Select a reason for imprisonment', href: '#imprisonment-status-0' }])
+    expect(result).toEqual([{ text: 'Select why this person is in prison', href: '#imprisonment-status-0' }])
   })
 
   it('should not return an error when an imprisonmentStatus is selected ', () => {
