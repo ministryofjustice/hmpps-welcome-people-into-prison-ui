c0c6ecab464932b9835300c881ba131de76e2fe2
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2022-04-21 06:58:23 +0100

    DCS-1574 added test to cover dob + prison number only

--- server/routes/bookedtoday/unexpectedArrivals/validation/searchForExistingRecordsValidation.test.ts
+++ server/routes/bookedtoday/unexpectedArrivals/validation/searchForExistingRecordsValidation.test.ts
@@ -17,6 +17,16 @@ describe('MatchedRecordSelectionValidation', () => {
       },
     ]))
 
+  it('Should return error if only DOB and prison number', () =>
+    expect(
+      SearchForExistingRecordsValidation({ day: '02', month: '01', year: '2000', prisonNumber: 'A1234BC' })
+    ).toEqual([
+      {
+        href: '#last-name',
+        text: "You must search using either the prisoner's last name and date of birth, prison number or PNC Number",
+      },
+    ]))
+
   it('Should return error if only first name entered', () =>
     expect(SearchForExistingRecordsValidation({ firstName: 'James' })).toEqual([
       { href: '#last-name', text: 'Enter a last name and a date of birth' },
