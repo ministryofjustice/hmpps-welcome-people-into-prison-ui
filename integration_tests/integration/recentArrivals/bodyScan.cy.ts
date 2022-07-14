4883d6a24859c06de820f07424535d1d1a5f99b7
Louise Norris <louise.norris@digital.justice.gov.uk>
2022-07-14 13:00:31 +0100

    DCS-1685 add integration test for current body scan implementation

---
+++ integration_tests/integration/recentArrivals/bodyScan.cy.ts
@@ -0,0 +1,39 @@
+import Role from '../../../server/authentication/role'
+import BodyScanPage from '../../pages/recentArrivals/bodyScan'
+import expectedArrivals from '../../mockApis/responses/expectedArrivals'
+
+const arrival = expectedArrivals.potentialMatch
+
+context('A user can record a body scan', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn', Role.PRISON_RECEPTION)
+    cy.task('stubPrison', 'MDI')
+    cy.task('stubAuthUser')
+    cy.task('stubUserCaseLoads')
+    cy.task('stubPrisonerDetails', arrival)
+  })
+
+  it('Should display error message when needed', () => {
+    cy.signIn()
+    const bodyScanPage = BodyScanPage.goTo(arrival.prisonNumber)
+
+    bodyScanPage.bodyScanTitleName().should('contain.text', 'Sam Smith')
+    bodyScanPage.submit().click()
+
+    bodyScanPage.hasError('Select a date for the body scan')
+
+    bodyScanPage.dateRadioButtons('another-date').click()
+    bodyScanPage.day().type('13')
+    bodyScanPage.month().type('07')
+    bodyScanPage.year().type('2022')
+    bodyScanPage.submit().click()
+
+    bodyScanPage.dateRadioButtons('another-date').should('be.checked')
+    bodyScanPage.day().should('have.value', '13')
+    bodyScanPage.month().should('have.value', '07')
+    bodyScanPage.year().should('have.value', '2022')
+
+    bodyScanPage.hasError('Select a reason for the body scan')
+  })
+})
