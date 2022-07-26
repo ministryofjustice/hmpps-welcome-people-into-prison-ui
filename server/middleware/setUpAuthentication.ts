421bde6fef51e9132050f53cf9bd5df5e9143e00
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2022-07-26 16:54:02 +0100

    DCS-1745 Bumping dependencies

--- server/middleware/setUpAuthentication.ts
+++ server/middleware/setUpAuthentication.ts
@@ -16,7 +16,7 @@ export default function setUpAuth(): Router {
 
   router.get('/autherror', (req, res) => {
     res.status(401)
-    return res.render('pages/autherror.njk')
+    return res.render('autherror')
   })
 
   router.get('/sign-in', passport.authenticate('oauth2'))
@@ -31,13 +31,13 @@ export default function setUpAuth(): Router {
   const authUrl = config.apis.hmppsAuth.externalUrl
   const authSignOutUrl = `${authUrl}/sign-out?client_id=${config.apis.hmppsAuth.apiClientId}&redirect_uri=${config.domain}`
 
-  router.use('/sign-out', (req, res) => {
+  router.use('/sign-out', (req, res, next) => {
     if (req.user) {
-      req.logout()
-      req.session.destroy(() => res.redirect(authSignOutUrl))
-      return
-    }
-    res.redirect(authSignOutUrl)
+      req.logout(err => {
+        if (err) return next(err)
+        return req.session.destroy(() => res.redirect(authSignOutUrl))
+      })
+    } else res.redirect(authSignOutUrl)
   })
 
   router.use('/account-details', (req, res) => {
