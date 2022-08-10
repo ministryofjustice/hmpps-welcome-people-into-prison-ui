caf6605bbaaece9f294f7e2e6624602aec4e4e07
Louise Norris <louise.norris@digital.justice.gov.uk>
2022-08-10 18:12:40 +0100

    DCS-1658 update temporary absence confirmation page

--- integration_tests/pages/temporaryabsences/confirmTemporaryAbsenceAddedToRoll.ts
+++ integration_tests/pages/temporaryabsences/confirmTemporaryAbsenceAddedToRoll.ts
@@ -19,21 +19,16 @@ export default class ConfirmTemporaryAbsenceAddedToRollPage extends Page {
       ),
   })
 
+  backToNewArrivals = () => ({
+    exists: () => Page.checkLink(cy.get('[data-qa=back-to-new-arrivals]'), 'Back to new arrivals', '/'),
+  })
+
   viewEstablishmentRoll = () => ({
     exists: () =>
       Page.checkLink(
         cy.get(`[data-qa=view-establishment-roll]`),
         'View establishment roll',
         'https://digital-dev.prison.service.justice.gov.uk/establishment-roll'
-      ),
-  })
-
-  backToDigitalPrisonServices = () => ({
-    exists: () =>
-      Page.checkLink(
-        cy.get('[data-qa=back-to-dps]'),
-        'Back to Digital Prison Services',
-        'https://digital-dev.prison.service.justice.gov.uk'
       ),
   })
 }
