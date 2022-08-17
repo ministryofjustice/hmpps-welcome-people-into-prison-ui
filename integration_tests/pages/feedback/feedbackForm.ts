0fa8e900983ab5a33acda93d7d935644e373303c
Louise Norris <louise.norris@digital.justice.gov.uk>
2022-08-17 14:27:45 +0100

    DCS-1702 feedback integration test

---
+++ integration_tests/pages/feedback/feedbackForm.ts
@@ -0,0 +1,18 @@
+import Page, { PageElement } from '../page'
+
+export default class FeedbackFormPage extends Page {
+  constructor() {
+    super('Give feedback on Welcome people into prison')
+  }
+
+  static goTo(): FeedbackFormPage {
+    cy.visit(`/feedback`)
+    return Page.verifyOnPage(FeedbackFormPage)
+  }
+
+  feedback = (): PageElement => cy.get('[data-qa="feedback"]')
+
+  email = (): PageElement => cy.get('[data-qa="email"]')
+
+  submit = (): PageElement => cy.get(`[data-qa=submit]`)
+}
