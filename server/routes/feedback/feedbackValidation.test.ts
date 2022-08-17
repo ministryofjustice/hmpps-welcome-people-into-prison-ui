20e2c6e46d8ae9b8c8d525ab9d14ad905aaf95b2
Louise Norris <louise.norris@digital.justice.gov.uk>
2022-08-17 17:10:35 +0100

    DCS-1702 removed unessesary lint comments

--- server/routes/feedback/feedbackValidation.test.ts
+++ server/routes/feedback/feedbackValidation.test.ts
@@ -24,10 +24,8 @@ describe('FeedbackValidator', () => {
     ['A@b@c@example.com'],
     ['a"b(c)d,e:f;g<h>i[jk]l@example.com'],
     ['just"not"right@example.com'],
-    // eslint-disable-next-line prettier/prettier, no-useless-escape
     ['this is"notallowed@example.com '],
-    // eslint-disable-next-line prettier/prettier, no-useless-escape
-    [' this also"notallowed@example.com'],
+    ['this still"notallowed@example.com'],
   ])('invalid email addresses : (%s)', email => {
     expect(FeedbackValidator({ feedback: 'Some content', email })).toEqual([
       {
