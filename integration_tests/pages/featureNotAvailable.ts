b1d8e89df6c9fe16b50c168f7674de22a76a1e92
Louise Norris <louise.norris@digital.justice.gov.uk>
2022-09-14 11:04:25 +0100

    DCS-1829 integration test fixes

--- integration_tests/pages/featureNotAvailable.ts
+++ integration_tests/pages/featureNotAvailable.ts
@@ -2,6 +2,6 @@ import Page from './page'
 
 export default class FeatureNotAvailablePage extends Page {
   constructor() {
-    super('You cannot confirm the arrival of this person in this tool')
+    super('You cannot confirm the arrival of this person in this service')
   }
 }
