d574905d1191aa4df39633189984e210d202dbad
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2022-09-16 12:07:14 +0100

    DCS-1852 Fixing trace logging in app insights

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,6 @@
+// Require app insights before anything else to allow for instrumentation of bunyan and express
+import './utils/azureAppInsights'
+
 import createApp from './app'
 import { services as wpipServicesBuilder } from './services'
 import { services as bodyScanServicesBuilder } from './bodyscan/services'
