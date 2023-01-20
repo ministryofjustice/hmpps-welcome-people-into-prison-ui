b90938dcf17e9872d85f599d352967ce2faa2bbf
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-01-20 10:01:10 +0000

    DCS-1932: ✨Add caption to prisoner summary page

--- server/routes/bookedtoday/summary.test.ts
+++ server/routes/bookedtoday/summary.test.ts
@@ -3,9 +3,9 @@ import request from 'supertest'
 import * as cheerio from 'cheerio'
 import { appWithAllRoutes } from '../__testutils/appSetup'
 import { createMockExpectedArrivalsService } from '../../services/__testutils/mocks'
-import { MatchType } from '../../services/matchTypeDecorator'
 import Role from '../../authentication/role'
 import { createArrival } from '../../data/__testutils/testObjects'
+import { MatchType } from '../../services/matchTypeDecorator'
 
 let app: Express
 const expectedArrivalsService = createMockExpectedArrivalsService()
@@ -39,5 +39,50 @@ describe('GET /prisoner/:id/summary', () => {
         const $ = cheerio.load(res.text)
         expect($('h1').text()).toContain('Smith, Jim')
       })
+  })
+
+  describe('caption', () => {
+    it('should render both PNC Number and Prison Number when both are given', () => {
+      return request(app)
+        .get('/prisoners/123/summary')
+        .expect(200)
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .expect(res => {
+          const $ = cheerio.load(res.text)
+          expect($('span.govuk-caption-l').text().trim()).toStrictEqual('Prison number: A1234AB | PNC: 01/98644M')
+        })
+    })
+
+    it('should render only Prison Number when only Prison Number is given', () => {
+      expectedArrivalsService.getArrival.mockResolvedValue({
+        ...createArrival({ pncNumber: null }),
+        matchType: MatchType.NO_MATCH,
+      })
+
+      return request(app)
+        .get('/prisoners/123/summary')
+        .expect(200)
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .expect(res => {
+          const $ = cheerio.load(res.text)
+          expect($('span.govuk-caption-l').text().trim()).toStrictEqual('Prison number: A1234AB')
+        })
+    })
+
+    it('should render only PNC Number when only PNC Number is given', () => {
+      expectedArrivalsService.getArrival.mockResolvedValue({
+        ...createArrival({ prisonNumber: null }),
+        matchType: MatchType.NO_MATCH,
+      })
+
+      return request(app)
+        .get('/prisoners/123/summary')
+        .expect(200)
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .expect(res => {
+          const $ = cheerio.load(res.text)
+          expect($('span.govuk-caption-l').text().trim()).toStrictEqual('PNC: 01/98644M')
+        })
+    })
   })
 })
