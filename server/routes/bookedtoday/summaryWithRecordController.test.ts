1179276ee1e025c4669d99bf697d9ba38c3baa2f
Louise N <louise.norris@digital.justice.gov.uk>
2023-02-01 08:52:30 +0000

    DCS-2023 pre arrival single match person summary (#371)
    * DCS-2023 pre arrival single match person summary
    
    * DCS-2023 use single potential match data for prison number, pnc number and image
    
    * DCS-2034 removed ids conditinal in summary with record view
    
    * DCS-2034 removed unecessary conditional

--- server/routes/bookedtoday/summaryWithRecordController.test.ts
+++ server/routes/bookedtoday/summaryWithRecordController.test.ts
@@ -19,7 +19,7 @@ afterEach(() => {
   jest.resetAllMocks()
 })
 
-describe('GET /prisoner/:id/summary', () => {
+describe('GET /prisoner/:id/summary-with-record', () => {
   beforeEach(() => {
     expectedArrivalsService.getArrival.mockResolvedValue(arrival)
   })
@@ -27,15 +27,15 @@ describe('GET /prisoner/:id/summary', () => {
   it('should call service method correctly', () => {
     expectedArrivalsService.getArrival.mockResolvedValue(arrival)
     return request(app)
-      .get('/prisoners/1111-1111-1111-1111/summary')
+      .get('/prisoners/1111-1111-1111-1111/summary-with-record')
       .expect(res => {
         expect(expectedArrivalsService.getArrival).toHaveBeenCalledWith('1111-1111-1111-1111')
       })
   })
 
-  it('should render summary page', () => {
+  it('should render summary-with-record page', () => {
     return request(app)
-      .get('/prisoners/1111-1111-1111-1111/summary')
+      .get('/prisoners/1111-1111-1111-1111/summary-with-record')
       .expect(200)
       .expect('Content-Type', 'text/html; charset=utf-8')
       .expect(res => {
@@ -47,7 +47,7 @@ describe('GET /prisoner/:id/summary', () => {
 
   it('should display breadcrumbs correctly', () => {
     return request(app)
-      .get('/prisoners/1111-1111-1111-1111/summary')
+      .get('/prisoners/1111-1111-1111-1111/summary-with-record')
       .expect(200)
       .expect('Content-Type', 'text/html; charset=utf-8')
       .expect(res => {
@@ -62,66 +62,27 @@ describe('GET /prisoner/:id/summary', () => {
   })
 
   describe('prisoner image', () => {
-    it('should render prisoner image when Prison Number provided', () => {
+    it('should render prisoner image with single potential match Prison Number', () => {
       return request(app)
-        .get('/prisoners/1111-1111-1111-1111/summary')
+        .get('/prisoners/1111-1111-1111-1111/summary-with-record')
         .expect(200)
         .expect('Content-Type', 'text/html; charset=utf-8')
         .expect(res => {
           const $ = cheerio.load(res.text)
-          expect($('[data-qa=prisoner-image]').attr('src')).toEqual('/prisoners/A1234AB/image')
-        })
-    })
-
-    it('should render placeholder image when no Prison Number provided', () => {
-      expectedArrivalsService.getArrival.mockResolvedValue(withMatchType(createArrival({ prisonNumber: null })))
-
-      return request(app)
-        .get('/prisoners/1111-1111-1111-1111/summary')
-        .expect(200)
-        .expect('Content-Type', 'text/html; charset=utf-8')
-        .expect(res => {
-          const $ = cheerio.load(res.text)
-          expect($('[data-qa=prisoner-image]').attr('src')).toEqual('/assets/images/placeholder-image.png')
+          expect($('[data-qa=prisoner-image]').attr('src')).toEqual('/prisoners/A5534HA/image')
         })
     })
   })
 
   describe('caption', () => {
-    it('should render both PNC Number and Prison Number when both are given', () => {
+    it('should render both PNC Number and Prison Number from single potential match', () => {
       return request(app)
-        .get('/prisoners/1111-1111-1111-1111/summary')
+        .get('/prisoners/1111-1111-1111-1111/summary-with-record')
         .expect(200)
         .expect('Content-Type', 'text/html; charset=utf-8')
         .expect(res => {
           const $ = cheerio.load(res.text)
-          expect($('span.govuk-caption-l').text().trim()).toStrictEqual('Prison number: A1234AB | PNC: 01/98644M')
-        })
-    })
-
-    it('should render only Prison Number when only Prison Number is given', () => {
-      expectedArrivalsService.getArrival.mockResolvedValue(withMatchType(createArrival({ pncNumber: null })))
-
-      return request(app)
-        .get('/prisoners/1111-1111-1111-1111/summary')
-        .expect(200)
-        .expect('Content-Type', 'text/html; charset=utf-8')
-        .expect(res => {
-          const $ = cheerio.load(res.text)
-          expect($('span.govuk-caption-l').text().trim()).toStrictEqual('Prison number: A1234AB')
-        })
-    })
-
-    it('should render only PNC Number when only PNC Number is given', () => {
-      expectedArrivalsService.getArrival.mockResolvedValue(withMatchType(createArrival({ prisonNumber: null })))
-
-      return request(app)
-        .get('/prisoners/1111-1111-1111-1111/summary')
-        .expect(200)
-        .expect('Content-Type', 'text/html; charset=utf-8')
-        .expect(res => {
-          const $ = cheerio.load(res.text)
-          expect($('span.govuk-caption-l').text().trim()).toStrictEqual('PNC: 01/98644M')
+          expect($('span.govuk-caption-l').text().trim()).toStrictEqual('Prison number: A5534HA | PNC: 11/836373L')
         })
     })
   })
--- server/routes/bookedtoday/summaryWithRecordController.ts
+++ server/routes/bookedtoday/summaryWithRecordController.ts
@@ -1,14 +1,15 @@
 import type { RequestHandler } from 'express'
 import { ExpectedArrivalsService } from '../../services'
 
-export default class SummaryController {
+export default class SummaryWithRecordController {
   public constructor(private readonly expectedArrivalsService: ExpectedArrivalsService) {}
 
   public view(): RequestHandler {
     return async (req, res) => {
       const { id } = req.params
       const arrival = await this.expectedArrivalsService.getArrival(id)
-      return res.render('pages/bookedtoday/summary.njk', { arrival })
+      const singleMatch = arrival.potentialMatches[0]
+      return res.render('pages/bookedtoday/summaryWithRecord.njk', { arrival, singleMatch })
     }
   }
 }
