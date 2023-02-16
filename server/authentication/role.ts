03fe3c6d47613fda94a3406db8ca359e01686fb8
GCheema <50441412+GurnankCheema@users.noreply.github.com>
2023-02-16 12:02:23 +0000

    DCS-1927 add link pointing to prisoner profile on dps (#386)
    * DCS-1927 add link to prisoner profile on dps
    
    * DCS-1927 change href so that it hits endpoint that automatically generates back link in DPS

--- server/authentication/role.ts
+++ server/authentication/role.ts
@@ -1,5 +1,6 @@
 const enum Role {
   PRISON_RECEPTION = 'ROLE_PRISON_RECEPTION',
+  ROLE_INACTIVE_BOOKINGS = 'ROLE_INACTIVE_BOOKINGS',
 }
 
 export default Role
--- server/routes/bookedtoday/summaryWithRecordController.ts
+++ server/routes/bookedtoday/summaryWithRecordController.ts
@@ -1,5 +1,6 @@
 import type { RequestHandler } from 'express'
 import { ExpectedArrivalsService } from '../../services'
+import Role from '../../authentication/role'
 
 export default class SummaryWithRecordController {
   public constructor(private readonly expectedArrivalsService: ExpectedArrivalsService) {}
@@ -7,8 +8,10 @@ export default class SummaryWithRecordController {
   public view(): RequestHandler {
     return async (req, res) => {
       const { id } = req.params
+      const { roles } = res.locals.user
+      const enableDpsLink = roles.includes(Role.PRISON_RECEPTION) && roles.includes(Role.ROLE_INACTIVE_BOOKINGS)
       const data = await this.expectedArrivalsService.getArrivalAndSummaryDetails(id)
-      return res.render('pages/bookedtoday/summaryWithRecord.njk', data)
+      return res.render('pages/bookedtoday/summaryWithRecord.njk', { ...data, enableDpsLink })
     }
   }
 }
