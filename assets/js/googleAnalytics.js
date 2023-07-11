40ca52ade97d3eae83bdf640f135b3e88ef7a1b1
David Thomas <david.thomas1@digital.justice.gov.uk>
2023-07-11 13:57:17 +0100

    MAP-34 Refactor GA4 config (#419)

--- assets/js/googleAnalytics.js
+++
@@ -1,9 +0,0 @@
-const el = document.getElementById('google-analytics')
-
-window.dataLayer = window.dataLayer || []
-function gtag() {
-  dataLayer.push(arguments)
-}
-gtag('js', new Date())
-
-gtag('config', `${el.dataset.analytics}`)
