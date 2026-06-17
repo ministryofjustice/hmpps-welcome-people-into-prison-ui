91f84f3fd29b38d66b5ebd43f052a698039482b4
GCheema <gurnank.cheema@justice.gov.uk>
2026-06-17 10:51:16 +0100

    MAPB-492 non-breaking security patch

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,11 +3,11 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // cypress is used for integration tests
-    'node_modules/cypress@15.16.0': 'ALLOW',
+    'node_modules/cypress@15.17.0': 'ALLOW',
     // Provides native integration, supporting the ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    'node_modules/esbuild@0.28.0': 'ALLOW',
+    'node_modules/esbuild@0.28.1': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Needed by esbuild for watching files during development
