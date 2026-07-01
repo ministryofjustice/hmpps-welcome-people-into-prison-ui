1f63bdfb18e335b3ac01b92b53407a44ba13a6e2
dafhoustonmoj <dafydd.houston@justice.gov.uk>
2026-07-01 17:17:19 +0100

    MAPB-581: fix integration tests for new health and ping stubbing

--- integration_tests/mockApis/wiremock.ts
+++ integration_tests/mockApis/wiremock.ts
@@ -13,4 +13,17 @@ const getMatchingRequests = body => superagent.post(`${url}/requests/find`).send
 const resetStubs = (): Promise<Array<Response>> =>
   Promise.all([superagent.delete(`${url}/mappings`), superagent.delete(`${url}/requests`), resetRedisDb()])
 
+export const stubPing = (urlPrefix: string, httpStatus = 200): SuperAgentRequest =>
+  stubFor({
+    request: {
+      method: 'GET',
+      urlPath: `${urlPrefix}/health/ping`,
+    },
+    response: {
+      status: httpStatus,
+      headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+      jsonBody: { status: httpStatus === 200 ? 'UP' : 'DOWN' },
+    },
+  })
+
 export { stubFor, getRequests, resetStubs, getMatchingRequests }
