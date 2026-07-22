c2814b9460514619d6ef294964f4177ca2869273
Dafydd Houston <159147179+dafhoustonmoj@users.noreply.github.com>
2026-07-22 10:59:47 +0100

    MAPB-630: Fix build number not being passed to App Insights (#722)
    * MAPB-630: fix build number not showing in app insights and update build info script. Add codeql gha
    
    * MAPB-630: vulnerability fixes

--- server/applicationVersion.ts
+++ server/applicationVersion.ts
@@ -1,9 +1,10 @@
-// eslint-disable import/no-unresolved,global-require
 import fs from 'fs'
+import path from 'path'
+import config from './config'
 
-const packageData = JSON.parse(fs.readFileSync('./package.json').toString())
-const buildNumber = fs.existsSync('./build-info.json')
-  ? JSON.parse(fs.readFileSync('./build-info.json').toString()).buildNumber
-  : packageData.version
+const packageData = JSON.parse(
+  fs.readFileSync(path.join(__dirname, `${__dirname.endsWith('/dist/server') ? '../' : ''}../package.json`)).toString(),
+)
+const { buildNumber } = config
 
 export default { buildNumber, packageData }
