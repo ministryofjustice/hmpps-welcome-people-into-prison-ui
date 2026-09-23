792fbba1361dbd84856731148bb3c863cf295bd4
Dan Benton <daniel.benton@justice.gov.uk>
2026-09-23 16:54:18 +0100

    Fix components stub

--- integration_tests/mockApis/components.ts
+++ integration_tests/mockApis/components.ts
@@ -1,46 +1,44 @@
 import { stubFor } from './wiremock'
 
-const stubComponents = () =>
-  stubFor({
-    request: {
-      method: 'GET',
-      urlPattern: '/components/components\\?component=header&component=footer',
-    },
-    response: {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      jsonBody: {
-        header: {
-          html: '<header><h1>Common Components Header</h1></header>',
-          javascript: [],
-          css: [],
-        },
-        footer: {
-          html: '<footer><h1>Common Components Footer</h1></footer>',
-          javascript: [],
-          css: [],
-        },
-      },
-    },
-  })
-
-const stubComponentsFail = () =>
-  stubFor({
-    request: {
-      method: 'GET',
-      urlPattern: '/components/components\\?component=header&component=footer',
-    },
-    response: {
-      status: 500,
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-      },
-    },
-  })
-
 export default {
-  stubComponents,
-  stubComponentsFail,
+  stubComponents: () => {
+    return stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/components/components\\?component=header&component=footer',
+      },
+      response: {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+        jsonBody: {
+          header: {
+            html: '<header><h1>Common Components Header</h1></header>',
+            javascript: [],
+            css: [],
+          },
+          footer: {
+            html: '<footer><h1>Common Components Footer</h1></footer>',
+            javascript: [],
+            css: [],
+          },
+        },
+      },
+    })
+  },
+  stubComponentsFail: () => {
+    return stubFor({
+      request: {
+        method: 'GET',
+        urlPattern: '/components/components\\?component=header&component=footer',
+      },
+      response: {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+      },
+    })
+  },
 }
